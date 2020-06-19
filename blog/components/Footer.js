4ded28d2bc67407dbeb13e1b489091e817a928ee
13033229336 <874609947@qq.com>
2020-06-19 15:08:49 +0800

    11

--- blog/components/Footer.js
+++ blog/components/Footer.js
@@ -2,7 +2,7 @@ import '../public/style/components/footer.css'
 const Footer = ()=>(
     <div className="footer-div">
         <div>系统由 React+Node+Ant Desgin驱动 </div>
-        <div>JSPang.com</div>
+        <div>beizou.com</div>
     </div>
 )
 
